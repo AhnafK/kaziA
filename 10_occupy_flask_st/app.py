757ad758c6d168ecf737e9d780ed43b50f028ece
AhnafK <akazi1@stuy.edu>
2018-09-24 23:54:09 -0400

    Added an untested homepage

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,4 +1,4 @@
-#Ahnaf Kazi
+#Brown Boys Sajed Nahian Ahnaf Kazi
 #SoftDev1 pd6
 #K10: Jinja Tuning
 #2018-09-24
@@ -12,6 +12,10 @@ app = Flask(__name__) # create instance of class Flask
 @app.route("/occupations") #assign fxn to route
 def occupations_page():
     return render_template('occupations.html', headings = hw06.headings, randomlySelectedJob = hw06.pickRandomOccupation(), occupations = hw06.occupationsDictionary)
+
+@app.route("/")
+def home_page():
+	return "<a href = '/occupations'>occupations</a>"
 
 if __name__ == "__main__":
     app.debug = True
