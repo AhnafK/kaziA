a4d915db1469281f8401cc2a31b2d96fd5c63b0e
AhnafK <akazi1@stuy.edu>
2018-11-14 00:06:21 -0500

    work done

---
+++ 24_rest/app.py
@@ -0,0 +1,20 @@
+#Ahnaf Kazi
+#SoftDev1 pd6
+#K 24: A RESTful Journey Skyward
+#2018-11-13
+
+from flask import Flask, render_template
+import json, urllib
+
+app = Flask(__name__) # create instance of class Flask
+
+#homeroute
+@app.route("/")
+def home_page():
+    f = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=bw5ysm0VMzQSxOx5E762RuVWkbyPUPwt98pJesBV").read()
+    url = json.loads(f)
+    return render_template(index.html, image=url)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
