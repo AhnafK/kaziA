0e7b3e6baa49c2f73298871909a9f84ee1a7bf7f
AhnafK <akazi1@stuy.edu>
2018-11-15 05:18:01 -0500

    finished

--- 25_rest/app.py
+++ 25_rest/app.py
@@ -11,12 +11,19 @@ app = Flask(__name__) # create instance of class Flask
 #homeroute
 @app.route("/")
 def home_page():
-    url = 'https://api.twitch.tv/helix/streams'
-    req = urllib.request.urlopen(url)
-    req = req.add_header("Client-ID","du81t8trvj2pz2ebio05eybxqgmdtq")
-    req = req.read()
-    dic = json.loads(req)
-    return render_template("index.html", image=dic["data"]["user_name"])
+    #url = 'https://api.twitch.tv/helix/streams'
+    #req = urllib.request.urlopen(url)
+    #req = req.add_header("Client-ID","du81t8trvj2pz2ebio05eybxqgmdtq")
+    #req = req.read()
+    url = 'https://api.twitch.tv/helix/streams?first=1'
+    req = urllib.request.Request(url)
+    req.add_header("Client-ID","du81t8trvj2pz2ebio05eybxqgmdtq")
+    resp = urllib.request.urlopen(req)
+    data = resp.read()
+    dic = json.loads(data)
+    #print(dic)
+    print(dic["data"][0]["user_name"])
+    return render_template("index.html", image=dic["data"][0]["user_name"])
 
 if __name__ == "__main__":
     app.debug = True
